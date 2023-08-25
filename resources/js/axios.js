4a9b60515093376294549389338b15e4341cefe3
Tiến Đỗ <tiendo@bravesoft.com.vn>
2023-08-25 17:29:55 +0700

    .

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -4,8 +4,29 @@ import Cookies from 'js-cookie';
 const axiosInstance  =  axios.create({
     baseURL: 'http://localhost:8000/api',
     headers: {
-        Authorization: 'Bearer ' + Cookies.get("access_token"),
         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     }
 })
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const access_token = Cookies.get("access_token")
+        console.log(access_token)
+        if (access_token) {
+            config.headers.Authorization = `Bearer ${access_token}`;
+        }
+        return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+)
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 export default axiosInstance;
